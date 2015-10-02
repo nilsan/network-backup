a53498136ded966ca5b3624e85380febb12e00ea
Ole-Morten Duesund <olemd@odinprosjekt.no>
2015-10-02 15:21:51 +0200

    add basic support for configuration-parsing and dumping

--- backup.rb
+++ backup.rb
@@ -3,6 +3,22 @@
 # A slightly more generalised alternative to RANCID - still pretty rancid though.
 
 require 'fileutils'
+require 'json'
+
+def read_configuration
+  # Read configuration from these files
+  config_files = ["default_config.json", "/etc/network-backup/config.json", "#{ENV["HOME"]}/.config/network-backup/config.json"]
+  configuration = {}
+  # Merge all configurations
+  config_files.each do |file|
+    if File.readable?(file)
+      c = JSON.parse(File.read(file))
+      configuration.merge!(c) if c
+    end
+  end
+  # Return complete configuration
+  configuration
+end
 
 def user_for(host)
   ENV["USERNAME"]
@@ -36,6 +52,21 @@ def perform_backup(host)
   system "git add ."
   system "git commit -m '#{commit_msg}'"
 end
+
+def dump_config(config)
+  puts "default : #{config["default"]}"
+
+  config["hosts"].each do |host|
+    hostconfig = config["default"]
+    if config["host-configuration"][host]
+      hostconfig.merge!(config["host-configuration"][host])
+    end
+    puts "#{host} : #{hostconfig}"
+  end
+end
+
+config = read_configuration
+dump_config(config)
 
 ARGV.each do |host|
   perform_backup(host)

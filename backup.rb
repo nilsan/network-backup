e4ab6975a6a6a65ed5658435626c6c12777e5cde
Ole-Morten Duesund <olemd@odinprosjekt.no>
2015-10-05 18:39:21 +0200

    refactor heavily, use git and net-ssh gems

--- backup.rb
+++ backup.rb
@@ -1,73 +1,143 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 
 # A slightly more generalised alternative to RANCID - still pretty rancid though.
 
 require 'fileutils'
 require 'json'
+require 'pp'
+require 'net/ssh'
+require 'git'
 
-def read_configuration
-  # Read configuration from these files
-  config_files = ["default_config.json", "/etc/network-backup/config.json", "#{ENV["HOME"]}/.config/network-backup/config.json"]
-  configuration = {}
-  # Merge all configurations
-  config_files.each do |file|
-    if File.readable?(file)
-      c = JSON.parse(File.read(file))
-      configuration.merge!(c) if c
+class Hash
+   def deep_merge(hash)
+      target = dup
+      hash.keys.each do |key|
+         if hash[key].is_a? Hash and self[key].is_a? Hash
+            target[key] = target[key].deep_merge(hash[key])
+            next
+         end
+         target[key] = hash[key]
+      end
+      target
+   end
+end
+
+module Network
+
+  class Backup
+
+    attr_reader :configuration
+    
+    class Configuration
+      
+      attr_reader :config
+      attr_reader :files
+      attr_reader :commands
+      attr_reader :configuration_files_used
+
+      def sanity_check(config_part, config_keys)
+        config_keys.each do |key|
+          abort("Missing key :#{key} in #{config_part}") if config_part[key].nil?
+        end
+      end
+      
+      def initialize
+        @files = ["default_config.json", "/etc/network-backup/config.json", "#{ENV["HOME"]}/.config/network-backup/config.json" ]
+        @config = {}
+        @configuration_files_used = []
+        @files.each do |file|
+          if File.readable?(file)
+            c = JSON.parse(File.read(file))
+            @config = @config.deep_merge(c) if c
+            @configuration_files_used << file
+          end
+        end
+
+        # Make sure we have the minimum of expected configuration-keys
+        sanity_check(@config, ["default"])
+        sanity_check(@config["default"], ["pool", "filter", "git", "user", "type", "login"])
+        
+        # And at least an empty hash for the per-host configuration
+        if @config["host-configuration"].nil?
+          @config["host-configuration"] = {}
+        end
+
+        # expand config["host-configuration][host] elements
+        # This way each host will have a complete
+        # configuration in @config["host-configuration"][hostname]
+        @config["hosts"].each do |host|
+          if @config["host-configuration"][host].nil?
+            @config["host-configuration"][host] = @config["default"]
+          else
+            @config["host-configuration"][host] = @config["default"].deep_merge(@config["host-configuration"][host])
+          end
+        end
+      end
+
+      def hosts
+        @config["hosts"]
+      end
+
+      def hostconfig(host)
+        @config["host-configuration"][host]
+      end
+
+      def commands(host)
+        @config["device-types"][hostconfig(host)["type"]]["commands"]
+      end
+
+      def author(host)
+        hostconfig(host)["git"]["author"]
+      end
+
     end
-  end
-  # Return complete configuration
-  configuration
-end
 
-def user_for(host)
-  ENV["USERNAME"]
-end
-
-def pool(host)
-  "#{ENV["HOME"]}/backup/network"
-end
-
-def target_repo(host)
-  pool(host) + "/" + host.split(".").reverse.join("/")
-end
-
-def commands_for(host)
-  ["version", "config", "chassis environment", "chassis firmware", "chassis hardware detail", "chassis alarm", "interfaces brief", "system boot-messages",
-   "system core-dumps", "system license", "vlans", "system commit"]
-end
-
-def perform_backup(host)
-  repo = target_repo(host)
-  commands = commands_for(host)
-  username = user_for(host)
-  FileUtils.mkpath(repo) unless Dir.exist?(repo)
-  abort unless Dir.chdir(repo)
-  commands.each do |command|
-    system "ssh #{username}@#{host} show #{command} > '#{command}'"
-  end
-  f = File.open("config", "r")
-  commit_msg = f.readline.strip
-  system "git init"
-  system "git add ."
-  system "git commit -m '#{commit_msg}'"
-end
-
-def dump_config(config)
-  puts "default : #{config["default"]}"
-
-  config["hosts"].each do |host|
-    hostconfig = config["default"]
-    if config["host-configuration"][host]
-      hostconfig.merge!(config["host-configuration"][host])
+    def initialize
+      @configuration = Configuration.new
     end
-    puts "#{host} : #{hostconfig}"
+
+    def perform_backups
+      @configuration.hosts.each do |host|
+        perform_backup(host, @configuration.hostconfig(host))
+      end
+    end
+
+    def perform_backup(host, config)
+      puts "Backing up #{host}"
+
+      repo = config["pool"] + "/" + host.split(".").reverse.join("/")
+      FileUtils.mkpath(repo) unless Dir.exist?(repo)
+      abort("Unable to change directory to #{repo}.") unless Dir.chdir(repo)
+
+      config["user"] = ENV["USER"] if config["user"].empty?
+
+      result = {}
+      Net::SSH.start(host, config["user"]) do |ssh|
+        @configuration.commands(host).each do |cmd|
+          result[cmd] = ssh.exec!("show #{cmd}")
+        end
+      end
+      result.each_pair do |k,v|
+        File.write(k, v)
+      end
+      git = Git.init
+      if ! (git.status.changed.empty? || git.status.untracked.empty?)
+        git.add(all: true)
+        git.commit_all("automatic backup", author: @configuration.author(host))
+      end
+    end
+
+    def dump_configuration
+      pp @configuration.configuration_files_used
+      pp @configuration.config
+    end
+    
   end
 end
 
-config = read_configuration
-dump_config(config)
+agent = Network::Backup.new
 
-ARGV.each do |host|
-  perform_backup(host)
-end
+agent.perform_backups
+
+#agent.dump_configuration
